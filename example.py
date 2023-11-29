ae0dcfecd68e1e50ce4d7b91626a953263ba0972
JACC <93591202+JACCCostring@users.noreply.github.com>
2023-11-29 14:49:27 +0100

    code works

---
+++ example.py
@@ -0,0 +1,25 @@
+class Login:
+    __dasdasd(username, password)
+    def logout(self): #logout()
+
+    def double_auth(self):
+
+    def logout_fake(self):
+        asdasdasdasdasd
+
+lo = Login('user', 'pass')
+lo.logout()
+
+
+
+def test_square():
+    obj = ShapeObject(5, 4)
+
+    assert obj.get_square() == True
+
+
+def test_valid_square():
+    obj = ShapeObject(5, 4)
+    square = obj.get_square()
+
+    assert obj.validate_square(square) == True
